237ef2ffbec7612bdaaea9d076492c568447e0e1
bijoymamud <bijoymamud.09@gmail.com>
2023-02-26 20:41:14 +0600

    finished spinner

--- js/app.js
+++ js/app.js
@@ -42,12 +42,28 @@ const displayPhones = (phones) => {
     
         `;
         phonesContainer.appendChild(phoneDiv);
-    })
+    });
+
+    // Stop Loader/spinner
+    toggleSpinner(false);
 }
 
 document.getElementById('btn-search').addEventListener('click', function () {
+
+    // star loader
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhone(searchText);
 })
+
+const toggleSpinner = (isLoading) => {
+    const loaderSection = document.getElementById('loader');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none')
+    }
+    else {
+        loaderSection.classList.add('d-none');
+    }
+}
 // loadPhone();
